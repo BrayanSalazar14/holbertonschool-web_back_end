d425c919baf328bc7a0495bd7a04b95c0118a92b
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-08 17:45:56 -0500

    hot fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -22,8 +22,6 @@ async def async_generator():
         async for number in async_generator():
             print(number)
     """
-    for _ in range(10):
+    for _ in range(11):
         await asyncio.sleep(1)
         yield uniform(0, 10)
-
-    data = [yield uniform(0, 10), await asyncio.sleep(1) for _ in range(0, 10)]
