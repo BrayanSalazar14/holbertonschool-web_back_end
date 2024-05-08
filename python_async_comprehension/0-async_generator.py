bba22bea362b6694d0ad5bfd2a0c53555ada320a
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-08 18:16:35 -0500

    hot fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ from random import uniform
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, float]:
     """
     Asynchronous generator that yields random floats between 0 and 10
     with a delay of 1 second between each yield.
