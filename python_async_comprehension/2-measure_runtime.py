4c0612b06e7178d4c95aef2c3a78acd0aab7815e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-09 13:59:22 -0500

    ultimate task

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+measure_runtime should measure the total runtime and return it.
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measure the total runtime for executing async_comprehension
+    four times concurrently.
+
+    Returns:
+        float: The total runtime in seconds.
+
+    Usage:
+        total_runtime = await measure_runtime()
+    """
+    start_time = time.time()
+    result = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*result)
+    return time.time() - start_time
