2ee8d06596cf62fb205e5fa3186dd407fef92f49
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-13 11:27:02 -0500

    new task v1

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module that changes all topics of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics field of a document in a MongoDB collection.
+
+    Args:
+        mongo_collection: A pymongo collection object.
+        name: The name of the document to be updated.
+        topics: The new list of topics to be set for the document.
+
+    Returns:
+        None
+    """
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_one({"name": name}, new_values)
