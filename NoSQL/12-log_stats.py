7cfab8f8ac8ca5924da49b3e47bd003b59a6c59f
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-13 12:51:16 -0500

    new task v2

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,31 +7,11 @@ if __name__ == "__main__":
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     my_db = myclient['logs']
     col = my_db['nginx']
-
-    get = 0
-    post = 0
-    put = 0
-    patch = 0
-    delete = 0
-    number_docs = 0
-
-    for doc in col.find():
-        if doc['method'] == 'GET':
-            get += 1
-        elif doc['method'] == 'POST':
-            post += 1
-        elif doc['method'] == 'PUT':
-            put += 1
-        elif doc['method'] == 'PATCH':
-            patch += 1
-        elif doc['method'] == 'DELETE':
-            delete += 1
-        number_docs += 1
-
-    print(f"{number_docs} logs")
-    print("Methods:")
-    print(f'\tmethod GET: {get}')
-    print(f'\tmethod POST: {post}')
-    print(f'\tmethod PUT: {put}')
-    print(f'\tmethod PATCH: {patch}')
-    print(f'\tmethod DELETE: {delete}')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print(f'{col.count_documents({})} logs')
+    print('Methods:')
+    for method in methods:
+        print('\t' + f'method {method}: ',
+              col.count_documents({'method': method}))
+    status_check = col.count_documents({'path': '/status'})
+    print(status_check, 'status check')
