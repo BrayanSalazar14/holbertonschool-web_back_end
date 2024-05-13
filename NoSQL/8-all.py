f121d27e5c93b8f8cbb38d0fbeec845044314982
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-13 10:57:15 -0500

    new task v1

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,6 +3,15 @@
 Module that lists all documents in a collection
 """
 
+if __name__ == "__main__":
+    def list_all(mongo_collection):
+        """
+    Retrieve all documents from a MongoDB collection.
 
-def list_all(mongo_collection):
-    return [col for col in mongo_collection.find({})]
+    Args:
+        mongo_collection: A pymongo collection object.
+
+    Returns:
+        A list containing all documents in the collection.
+    """
+        return [col for col in mongo_collection.find({})]
