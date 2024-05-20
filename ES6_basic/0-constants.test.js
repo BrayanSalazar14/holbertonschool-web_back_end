858353e00702214e25e00ac6ba5f02b71b269050
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-20 13:15:50 -0500

    new task v1

---
+++ ES6_basic/0-constants.test.js
@@ -0,0 +1,5 @@
+import { taskFirst, taskNext } from './0-constants.js';
+
+test('task are correctly', () => {
+    expect(`${taskFirst()} ${taskNext()}`).toEqual('I prefer const when I can. But sometimes let is okay')
+})
