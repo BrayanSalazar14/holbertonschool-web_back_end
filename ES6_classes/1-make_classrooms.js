eb0db945b565f12042da90d89da93c411c00a9a0
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-21 17:11:44 -0500

    task v2

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import ClassRoom from "./0-classroom";
+/* eslint-disable */
 
 function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
--- ES6_classes/eslint.config.mjs
+++ ES6_classes/eslint.config.mjs
@@ -1,8 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-];
+export { globals }; // Si necesitas exportar las globales por alguna razón específica
+export default {
+  languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  ...pluginJs.configs.recommended,
+};
