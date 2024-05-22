4edb02a7964e4744f7dfe9c43917d9c17042bb27
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-22 15:08:33 -0500

    new task v1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,7 +2,7 @@
 import ClassRoom from "./0-classroom";
 /* eslint-disable */
 
-function initializeRooms() {
+export default function initializeRooms() {
   const roomsSize = [19, 20, 34]
   const rooms = roomsSize.map((size) => new ClassRoom(size))
   return rooms
