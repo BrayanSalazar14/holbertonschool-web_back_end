f85b961ea8dabc3c386787cda112103ca66c2bab
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-22 17:10:43 -0500

    new task v1

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      throw Error('Class extending Building must override evacuationWarningMessage')
+    } else {
+      this.sqft = sqft;
+    }
+  }
+
+  get sqtf() {
+    return this._sqtf;
+  }
+
+  set sqft(value) {
+    this._sqtf = value;
+  }
+}
