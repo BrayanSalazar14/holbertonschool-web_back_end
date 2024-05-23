0e858b92dfa034ba29574ba54feb2e2e069639d1
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-23 16:31:14 -0500

    new task v1

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,30 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+/* eslint-disable */
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    } if (hint === 'string') {
+      return this._location;
+    }
+  }
+}
