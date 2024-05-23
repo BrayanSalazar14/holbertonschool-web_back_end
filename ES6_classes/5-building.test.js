f4477e1b04fee022fb404f5987140c84d434085a
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-23 15:44:27 -0500

    new task v1

--- ES6_classes/5-building.test.js
+++
@@ -1,9 +0,0 @@
-import Building from './5-building.js';
-
-class TestBuilding extends Building {}
-
-test("Building forces override", () => {
-  expect(() => {
-    new TestBuilding(200);
-  }).toThrowError("Class extending Building must override evacuationWarningMessage");
-});
