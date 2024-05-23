8028403dc64d8aa7d92074211426eecbfb91648e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-23 15:10:57 -0500

    hot fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,30 +1,22 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building) {
-      this.evacuationWarningMessage();
+    if (this.constructor !== Building) {
+      throw new Error('Cannot instantiate abstract class Building directly');
     } else {
-      this._sqft = sqft;
+      this.sqft = sqft;
     }
   }
 
   get sqtf() {
-    return this.sqtf;
+    return this._sqtf;
   }
 
+  set sqft(value) {
+    this._sqtf = value;
+  }
   /* eslint-disable */
   evacuationWarningMessage() {
     throw Error('Class extending Building must override evacuationWarningMessage');
   }
+  /* eslint-disable */
 }
-
-// const b = new Building(100);
-// console.log(b);
-
-// class TestBuilding extends Building {}
-
-// try {
-//     new TestBuilding(200)
-// }
-// catch(err) {
-//     console.log(err);
-// }
