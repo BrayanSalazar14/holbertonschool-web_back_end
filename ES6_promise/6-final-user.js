f67aedf3bff1166ded026d3e946faa2bb8d0c068
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-28 16:29:19 -0500

    hot fix, test

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,6 @@ import signUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUser(firstName, lastName), uploadPhoto(fileName)])
+  .then(value => console.log(value))
 }
