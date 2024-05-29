5b3f4a10b390f08db6f1ab4f49a2834d2dd73028
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-29 14:28:26 -0500

    new task v1

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,9 @@
+export default function guardrail(mathFunction) {
+    const arr = new Array
+    try {
+        arr.map((value) => mathFunction)
+    }
+    catch {
+        arr.map((value) => mathFunction)
+    }
+}
