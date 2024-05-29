49d7df99b0076c168006f7d84ba43eaa068d9c09
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-29 10:45:30 -0500

    interpolacion

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,15 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const {
-        body,
-      } = values[0];
-      const {
-        firstName,
-        lastName,
-      } = values[1];
-      console.log(body, firstName, lastName);
-    })
+    .then(([photo, user]) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
