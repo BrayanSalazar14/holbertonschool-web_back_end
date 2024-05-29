617f0c1e017881a10c687b5ff1c60dcd2b85330f
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-29 15:08:07 -0500

    hot fix

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,8 +1,18 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    return {
-        photo: await uploadPhoto(),
-        user: await createUser()
+    const data = {}
+    try {
+        data.photo = await uploadPhoto()
+        data.user = await createUser()
     }
+    catch {
+        data.photo = null
+        data.photo = null
+    }
+    return data
+    // return {
+    //     photo: await uploadPhoto(),
+    //     user: await createUser()
+    // }
 }
