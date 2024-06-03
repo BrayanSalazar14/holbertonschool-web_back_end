0a104102fb6741adc30a7fd6a87b35b6e99e3b4e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-03 12:42:30 -0500

    new task v1

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  const results = array.map((element) => set.has(element));
-  if (results.includes(false)) return false;
-  return true;
+  return array.every((element) => set.has(element));
 }
