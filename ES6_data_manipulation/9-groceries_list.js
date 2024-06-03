c8b10308e8cb139364dd5330950962c4505044c5
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-03 14:01:33 -0500

    new task v1

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const newItem = new Map();
+  newItem.set('Apples', 10);
+  newItem.set('Tomatoes', 10);
+  newItem.set('Pasta', 1);
+  newItem.set('Rice', 1);
+  newItem.set('Banana', 5);
+  return newItem;
+}
