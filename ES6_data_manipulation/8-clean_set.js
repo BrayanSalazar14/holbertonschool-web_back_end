59a614dd1a3aa17aea468fd174ffeae9c1bba1bb
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-05 09:56:17 -0500

    Add method join for array for return un newString formated

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!(startString instanceof String)) return '';
   const newString = [];
   set.forEach((element) => {
     if (element.startsWith(startString)) newString.push(element.slice(startString.length));
