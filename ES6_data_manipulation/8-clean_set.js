ffce4feaf2800da633f6236d031ff3a7c24daeba
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-05 09:05:29 -0500

    new task v1

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+// export default function cleanSet(set, startString) {
+//     let newString = ''
+//     set.forEach(element => {
+//         if (element.includes(startString))
+//             {
+//                 const ele = element.slice(startString.length)
+//                 newString += ele
+//             }
+//         })
+//     return newString
+// }
